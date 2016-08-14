a2bad08c1e2ddf644bfd986ac45ae6d99b543946
Michael Robinson <seraph890@gmail.com>
2016-08-14 23:07:17 +1000

    added initial test

--- flatten_arrays/test/flatten_spec.rb
+++ flatten_arrays/test/flatten_spec.rb
@@ -1,12 +1,17 @@
-
-require_relative "../scripts/flatten"
 require "test/unit"
 require "pry"
+require_relative "../flatten"
 
 class TestSimpleNumber < Test::Unit::TestCase
 
-  def test_all_things
-    binding.pry
+  def setup
+    flatten = Flatten_arrays.new
   end
 
+  def should_combine_nested_arrays
+    all = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    array = [1, 2, [3, 4], 5, [6, 7, [8, 9]]]
+
+    assert_equal all, flatten.combine_nested_arrays(array)
+  end
 end
