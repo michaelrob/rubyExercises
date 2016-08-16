e98506dd57c16c259cddc9f77522d62e2075dd7d
Michael Robinson <seraph890@gmail.com>
2016-08-16 21:17:05 +1000

    even more specs

--- flatten_arrays/spec/flatten_spec.rb
+++ flatten_arrays/spec/flatten_spec.rb
@@ -8,9 +8,20 @@ describe '#flatten_nested_arrays' do
   end
 
   it 'should combine nested arrays' do
-    numbers = [1,2,[3,4,5],6,[7,[8,9]]]
-    flatten_array = @flatten.flatten_nested_arrays(numbers)
+    numbers = [1,2,[3,4,5],6,[7,8,9]]
 
-    expect(flatten_array).to eq([1,2,3,4,5,6,7,8,9])
+    expect(@flatten.flatten_nested_arrays(numbers)).to eq([1,2,3,4,5,6,7,8,9])
+  end
+
+  it 'should combine nested arrays with nested arrays' do
+    numbers = [1,2,[3,[4,5]],[6,[7,8,9]]]
+
+    expect(@flatten.flatten_nested_arrays(numbers)).to eq([1,2,3,4,5,6,7,8,9])
+  end
+
+  it 'should combine nested arrays with multiple data types' do
+    numbers = [1,"two",[3,4,5],6,[7,8.2,9]]
+
+    expect(@flatten.flatten_nested_arrays(numbers)).to eq([1,"two",3,4,5,6,7,8.2,9])
   end
 end
