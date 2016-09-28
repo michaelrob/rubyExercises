5aaac6e1512416010080d8de78e118fef7a85fdd
Michael Robinson <seraph890@gmail.com>
2016-09-28 10:24:08 +1000

    added additional let to sort service spec

--- word_challenge/spec/word_sort_service_spec.rb
+++ word_challenge/spec/word_sort_service_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe WordSortService do
 
   let!(:sort_service) { WordSortService.new }
   let(:word) { "carrots" }
+  let(:expected_result) { ["carr", "arro", "rrot", "rots"] }
 
   describe 'find_sequences' do
     it 'should return sequences found in word' do
