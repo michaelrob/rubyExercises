2e89f1ed0d10faf91727f71944810d854a8b0756
Michael Robinson <seraph890@gmail.com>
2016-09-28 10:05:05 +1000

    Fleshed out specs a bit more

--- word_challenge/spec/word_challenges_integration_spec.rb
+++ word_challenge/spec/word_challenges_integration_spec.rb
@@ -13,14 +13,19 @@ describe "integration" do
   }
 
   let(:result) { WordChallenge.call(dictionary) }
+  let(:expected_result) { [["arrow", "rrow"], ["arrow", "rows"], ["carrots", "carr"], ["carrots", "rrot"], ["carrots", "rots"], ["give", "give"]] }
 
   describe WordChallenge do
-    it "outputs list of sequences and words" do
-
+    it 'outputs list of sequences and words' do
+      expect(result).to eq()
     end
 
-    it "should not contain duplicate sequences" do
+    it 'should not contain duplicate sequences' do
+      expect(results).not_to include("me")
+    end
 
+    it 'should contain sequence for each word' do
+      expect(expected_result.length).to be(6)
     end
   end
 
