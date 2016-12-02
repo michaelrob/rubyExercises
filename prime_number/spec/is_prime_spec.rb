9b92722130174d3fe3e03a435051927902bbe883
Michael Robinson <seraph890@gmail.com>
2016-12-02 20:46:24 +0900

    updated specs, added basic spec for prime number testing

--- prime_number/spec/is_prime_spec.rb
+++ prime_number/spec/is_prime_spec.rb
@@ -1,34 +1,10 @@
 require 'spec_helper'
-require 'flatten'
+require 'is_prime'
 
 describe Flatten_Arrays do
-  it 'should combine nested arrays' do
-    numbers = [1,2,[3,4,5],6,7,8,9]
+  it 'should return true for prime number' do
+    number = 11
 
-    expect(Flatten_Arrays.call(numbers)).to eq([1,2,3,4,5,6,7,8,9])
-  end
-
-  it 'should combine multiple nested arrays' do
-    numbers = [1,2,[3,4,5],[6,7,8],9]
-
-    expect(Flatten_Arrays.call(numbers)).to eq([1,2,3,4,5,6,7,8,9])
-  end
-
-  it 'should combine nested arrays with nested arrays' do
-    numbers = [1,2,[3,[4,5]],6,7,8,9]
-
-    expect(Flatten_Arrays.call(numbers)).to eq([1,2,3,4,5,6,7,8,9])
-  end
-
-  it 'should combine multiple nested arrays with multplie nested arrays' do
-    numbers = [1,2,[3,[4,5]],[6,[7,[8,9]]]]
-
-    expect(Flatten_Arrays.call(numbers)).to eq([1,2,3,4,5,6,7,8,9])
-  end
-
-  it 'should combine nested arrays with multiple data types' do
-    numbers = [1,"two",[3,4,5],6,[7,8.2,9]]
-
-    expect(Flatten_Arrays.call(numbers)).to eq([1,"two",3,4,5,6,7,8.2,9])
+    expect(Is_Prime(number)).to eq(true)
   end
 end
