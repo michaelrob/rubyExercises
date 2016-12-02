f1d8e73536fe18e98f22de83bda228b6acc6d3f0
Michael Robinson <seraph890@gmail.com>
2016-12-02 20:50:21 +0900

    got basic spec working... now to add more functionality

--- prime_number/lib/is_prime.rb
+++ prime_number/lib/is_prime.rb
@@ -1,10 +1,12 @@
 class Is_Prime
   attr_reader :all
 
-  def self(num)
+  def self.call(num)
     for i in 2..num-1
       if num % i == 0
         is_prime = false
+      else
+        return true
       end
     end
   end
--- prime_number/spec/is_prime_spec.rb
+++ prime_number/spec/is_prime_spec.rb
@@ -1,10 +1,10 @@
 require 'spec_helper'
 require 'is_prime'
 
-describe Flatten_Arrays do
+describe Is_Prime do
   it 'should return true for prime number' do
     number = 11
 
-    expect(Is_Prime(number)).to eq(true)
+    expect(Is_Prime.call(number)).to eq(true)
   end
 end
